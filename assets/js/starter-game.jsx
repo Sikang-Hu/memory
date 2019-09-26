eed3fbe8a805e52682cf6e6f4be95405d12979d7
sikang hu <sikang_hu@berkeley.edu>
2019-09-25 20:44:12 -0400

    finished

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -6,42 +6,148 @@ export default function game_init(root) {
   ReactDOM.render(<Starter />, root);
 }
 
+
+
+function random_list() {
+  let arr = Array.from("ABCDEFGH");
+  arr = arr.concat(arr);
+  return shuffle(arr);
+}
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let x = arr[i];
+    arr[i] = arr[j];
+    arr[j] = x;
+  }
+}
+
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    this.state = {
+      tiles: random_list(),
+      clicked: [],
+    };
   }
 
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
+  restart() {
+    let init = _.extend(this.state, {
+      clicked: [],
+    });
+    this.setState(init);
   }
 
-  hax(_ev) {
-    alert("hax!");
+  clicks() {
+    return this.state.clicked.length;
+  }
+
+  completed () {
+    let clk = this.state.clicked;
+    let last =  clk.length % 2 == 0 ? clk.length : clk.length - 1;
+    let result = [];
+    for (let i = 0; i < last; i += 2) {
+      if (this.state.tiles[clk[i]] == this.state.tiles[clk[i + 1]]) {
+        result.push(this.state.tiles[clk[i]]);
+      }
+    }
+    return result;
+  }
+
+  currentBoard() {
+    let clk = this.state.clicked;
+    let comp = this.completed();
+    let result = this.state.tiles.map((t) => {
+      return comp.includes(t) ? t : " ";
+    });
+    if (clk.length % 2 != 0) {
+      result[clk.length - 1] = this.tiles[clk.length - 1];
+    }
+    return result;
+  }
+
+  handler(i) {
+    let clk = this.state.clicked;
+    let t = this.state.tiles;
+    let comp = this.completed();
+    if (comp.includes(t[i])) {
+      return;
+    }
+    clk.push(i);
   }
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
-
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
-    }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    }
+    return (
+      <div className="row">
+        <div className="column">
+          <h1>Memory Game &#129409</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column column-50 column-offset-25">
+          <Board root={this} onClick={(i) => this.handler(i)}/>
+        </div>
+      </div> 
+      <div className="row">
+        <div className="column">
+          <Score root={this} />
+        </div>
+        <div className="column">
+          <p><button onClick={this.restart.bind(this)}>Restart</button></p>
+        </div>
+      </div> 
+    );
   }
+}
+
+class Board extends React.Component {
+  renderTile(board, i){
+    return (<Tile 
+            key={i}
+            value={board[i]} 
+            onClick={() => this.props.onClick(i)}
+          />);
+  }
+
+  renderRow(borad, row, columns) {
+    let tiles = [];
+    for (var i = 0; i < columns; i++) {
+      rows.push(this.renderTile(board, i + 4 * row));
+    }
+    return (
+      <div className="row" key={"row-" + row}>
+        {tiles}
+      </div>
+      );
+  }
+
+  render () {
+    let cur = this.props.root.currentBoard();
+    let r = [];
+    for (var i = 0; i < 4; i++) {
+      r.push(this.renderRow(i, 4, cur));
+    }
+    return (
+      <div className="board">
+        {r}
+      </div>
+      );
+  }
+}
+
+function Tile(params) {
+  return (
+    <button className="tile" onClick={params.onClick}>
+      {params.value}
+    </button>
+    );
+}
+
+function Score(params) {
+  return (<div>
+    <p><strong>Clicks:</strong></p>
+    <p>{params.root.clicks()}</p>
+    </div>);
 }
 
